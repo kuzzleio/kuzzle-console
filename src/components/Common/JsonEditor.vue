d422b2194d2c57533deef7e32dad122cfd0cc24c
Berthier <Berthier.est@gmail.com>
2020-10-02 16:30:54 +0200

    monaco editor

--- src/components/Common/JsonEditor.vue
+++ src/components/Common/JsonEditor.vue
@@ -1,32 +1,23 @@
 <template>
-  <div
+  <!-- <div
     data-cy="JSONEditor"
     ref="jsoneditor"
     :class="classes"
     :id="id"
     :style="style"
-  />
+  /> -->
+  <MonacoEditor
+    width="600"
+    :height="height"
+    theme="vs-light"
+    :options="options"
+    :value="content"
+  ></MonacoEditor>
 </template>
-
-<style lang="scss" rel="stylesheet/scss">
-.ace_text-input {
-  position: relative;
-}
-
-.ace-tomorrow.ace_editor.readonly {
-  background-color: #d6d6d6;
-  .ace_gutter,
-  .ace_active-line {
-    background-color: #d6d6d6;
-  }
-  .ace_selection {
-    background: #a7c4de;
-  }
-}
-</style>
-
 <script>
-import Vue from 'vue'
+import MonacoEditor from 'monaco-editor-vue'
+
+// import Vue from 'vue'
 let editor
 
 export default {
@@ -45,7 +36,24 @@ export default {
       default: false
     }
   },
+  components: {
+    MonacoEditor
+  },
   computed: {
+    options() {
+      return {
+        lineNumbers: "true",
+        acceptSuggestionOnEnter: "on",
+        autoClosingBrackets: true,
+        autoClosingQuotes: true,
+        autoIndent: "full",
+        language: "javascript",
+        colorDecorators: true,
+        columnSelection: true,
+        formatOnType: true,
+        tabCompletion: "on"
+      }
+    },
     classes() {
       return (this.readonly ? 'readonly ' : '') + this.myclass
     },
@@ -58,6 +66,9 @@ export default {
     }
   },
   methods: {
+    onChange(value) {
+      console.log(value)
+    },
     getRawValue() {
       return editor.getValue()
     },
@@ -70,37 +81,53 @@ export default {
     }
   },
   watch: {
-    refreshAce() {
-      setTimeout(() => {
-        editor.focus()
-      }, 500)
-    }
+    // refreshAce() {
+    //   setTimeout(() => {
+    //     editor.focus()
+    //   }, 500)
+    // }
   },
   mounted() {
-    Vue.nextTick(() => {
-      /* eslint no-undef: 0 */
-      editor = ace.edit(this.$refs.jsoneditor, {
-        mode: 'ace/mode/json'
-      })
-      editor.setTheme('ace/theme/tomorrow')
-      editor.setFontSize(15)
-      editor.getSession().setTabSize(2)
-      editor.setReadOnly(this.readonly)
-      editor.$blockScrolling = Infinity
-      this.setContent(this.content)
-
-      // WARNING - Beware of update loops!
-      // This event is triggered both when the content changes after
-      // user interaction and when it is set programmatically.
-      editor.on('change', () => {
-        this.$emit('change', this.getRawValue())
-      })
-    })
-  },
-  beforeDestroy() {
-    if (editor) {
-      editor.removeAllListeners('change')
-    }
+    // Vue.nextTick(() => {
+    //   /* eslint no-undef: 0 */
+    //   editor = ace.edit(this.$refs.jsoneditor, {
+    //     mode: 'ace/mode/json'
+    //   })
+    //   editor.setTheme('ace/theme/tomorrow')
+    //   editor.setFontSize(15)
+    //   editor.getSession().setTabSize(2)
+    //   editor.setReadOnly(this.readonly)
+    //   editor.$blockScrolling = Infinity
+    //   this.setContent(this.content)
+    //   // WARNING - Beware of update loops!
+    //   // This event is triggered both when the content changes after
+    //   // user interaction and when it is set programmatically.
+    //   editor.on('change', () => {
+    //     this.$emit('change', this.getRawValue())
+    //   })
+    // })
   }
+  // beforeDestroy() {
+  //   if (editor) {
+  //     editor.removeAllListeners('change')
+  //   }
+  // }
 }
 </script>
+
+<style lang="scss" rel="stylesheet/scss">
+// .ace_text-input {
+//   position: relative;
+// }
+
+// .ace-tomorrow.ace_editor.readonly {
+//   background-color: #d6d6d6;
+//   .ace_gutter,
+//   .ace_active-line {
+//     background-color: #d6d6d6;
+//   }
+//   .ace_selection {
+//     background: #a7c4de;
+//   }
+// }
+</style>
